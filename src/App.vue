6aaaeac22ccce759141c8c89e28d754073aa96fd
archdev <dev@archlinux.com>
2021-04-12 20:24:01 +0800

    refactor navbar

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
-
-        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <!-- <h-property entity="user" entity-id="6071614ca73a602476c92d41"></h-property> -->
-      <!-- <h-search-form entity="user" :fields="search_fields" :cols="6" clear-label="reset" search-label="query" v-model="form" @search="do_search"></h-search-form> -->
-      <!-- <h-edit-form entity="user" v-model="form" hide-cancel :cols="6" :fields="search_fields" :success-hint="success_hint" submit-label="Save" @saved="saved"></h-edit-form> -->
-      <!-- <h-table searchable entity="user" :headers="headers" :sort-key="sort_key" :sort-desc="sort_desc" :search-fields="search_fields" :search-cols="6" clear-label="reset" search-label="query"></h-table> -->
-      <h-crud searchable mode="cu" dialog-width="900px" :headers="headers" :edit-fields="search_fields" :filter="filter" entity="user" header-align="end" item-label-key="name" :sort-key="sort_key" :sort-desc="sort_desc" :search-cols="4" :cols="6"></h-crud>
+      <h-nav-bar :items="menus" exit_url="/logout"></h-nav-bar>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import { no_value } from "./core/type";
-
 export default {
-  name: "App",
-
   components: {},
-
-  data: () => ({
-    filter: { age: ">30" },
-    form: {},
-    success_hint: "you have successfully registered as a new user",
-    sort_key: ["name"],
-    sort_desc: [false],
-    search_fields: [
-      { name: "name", icon: "mdi-account" },
-      { name: "role", icon: "mdi-account" },
-      { name: "gender", icon: "mdi-account" },
-      { name: "age", icon: "mdi-account" },
-      { name: "currency", icon: "mdi-account" },
-      {
-        name: "rate",
-        icon: "mdi-account",
-        rule: [(value) => no_value(value) || value <= 100 || "err value for rate"],
-      },
-    ],
-    headers: [
-      { name: "name" },
-      { name: "email" },
-      {
-        name: "age",
-        style: (value) => {
-          if (value > 15) {
-            return "red--text";
-          } else {
-            return "green--text";
-          }
+  name: "App",
+  data() {
+    return {
+      menus: [
+        {
+          title: this.$t("app.cloud_management"),
+          icon: "mdi-apps",
+          items: [
+            { icon: "mdi-apps", title: this.$t("app.host_management"), route: "/host" },
+            { icon: "mdi-tag-outline", title: this.$t("app.workload_management"), route: "/workload" },
+          ],
         },
-      },
-      {
-        name: "role",
-        align: "center",
-        chip: true,
-        style: () => {
-          return "blue";
+        {
+          title: this.$t("app.system_management"),
+          icon: "mdi-apps",
+          items: [
+            { icon: "mdi-account-key", title: this.$t("app.user_management"), route: "/user" },
+            { icon: "mdi-alert-box", title: this.$t("app.system_log"), route: "/log" },
+          ],
         },
-      },
-      { name: "gender" },
-      { name: "birthday" },
-      { name: "rate" },
-    ],
-  }),
-
-  // created() {
-  //   console.log(this.$te("user.role.hint"));
-  // },
-
-  methods: {
-    cancel_search() {
-      console.log("cancel_search");
-      console.log(this.form);
-    },
-
-    do_search() {
-      console.log("do_search");
-      console.log(this.form);
-    },
-
-    saved() {
-      console.log("after saving");
-      console.log(this.form);
-    },
+      ],
+    };
   },
 };
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,30 +1,29 @@
 <template>
   <nav>
-    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app dark flat class="primary">
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app dark flat :color="barColor">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
         <span class="hidden-sm-and-down">{{ app_title }}</span>
       </v-toolbar-title>
       <v-spacer />
       <slot name="toolbar" />
-      <v-btn icon>
-        <v-icon @click="exit_to_app">exit_to_app</v-icon>
-      </v-btn>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
-      <v-list v-for="item in items" :key="item.title">
-        <v-list subheader dense>
-          <v-subheader>{{ item.title }}</v-subheader>
+    <v-navigation-drawer v-bind="$attrs" v-on="$listeners" v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app :width="drawerWidth">
+      <v-list>
+        <v-list-group v-for="item in items" :key="item.title" v-model="item.active" :prepend-icon="item.icon" no-action>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+
           <v-list-item v-for="child in item.items" :key="child.title" router :to="child.route">
             <v-list-item-icon>
               <v-icon v-text="child.icon"></v-icon>
             </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="child.title"></v-list-item-title>
-            </v-list-item-content>
+            <v-list-item-title v-text="child.title"></v-list-item-title>
           </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
   </nav>
@@ -36,9 +35,9 @@ export default {
 
   props: {
     items: { type: Array, required: true },
-    exit_url: { type: String, required: true },
     title: { type: String },
-    index: { type: String, default: "index" },
+    barColor: { type: String, default: "teal" },
+    drawerWidth: { type: Number, default: 300 },
   },
 
   data() {
@@ -54,16 +53,6 @@ export default {
       } else {
         return this.$t("app.title");
       }
-    },
-  },
-
-  methods: {
-    exit_to_app() {
-      this.$axios_post(this.exit_url).then((result) => {
-        if (this.is_success_reponse(result.code)) {
-          this.$router.push({ name: this.index });
-        }
-      });
     },
   },
 };
