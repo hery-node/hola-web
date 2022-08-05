19a950e52f4f570c8adfd6c8cef43ed09d711270
Heyuan Liu <dev@dev.com>
2022-08-05 08:59:27 +0800

    refactor names and dependencies

--- src/components/index.js
+++ src/components/index.js
@@ -2,30 +2,30 @@ import Vue from "vue";
 import { init_axios, get_url, axios_get, axios_post, axios_cached_get, axios_download, axios_upload, is_success_response, is_error_response, is_been_referred, is_duplicated, has_invalid_params, is_no_session, save_entity, read_entity, read_entity_properties, list_entity, delete_entity, get_ref_labels, get_entity_meta } from "../core/axios";
 import { register_type, get_type, no_value, is_int } from "../core/type";
 
-import Confirm from "./Confirm.vue";
-import Form from "./Form.vue";
-import ArrayList from "./Array.vue";
-import Compare from "./Compare.vue";
+import ConfirmDialog from "./ConfirmDialog.vue";
+import BasicForm from "./BasicForm.vue";
+import ArrayTable from "./ArrayTable.vue";
+import CompareTable from "./CompareTable.vue";
 import CompareEntity from "./CompareEntity.vue";
 import SearchForm from "./SearchForm.vue";
 import EditForm from "./EditForm.vue";
-import Table from "./Table.vue";
-import Crud from "./Crud.vue";
-import Property from "./Property.vue";
-import Navbar from "./Navbar.vue";
+import DataTable from "./DataTable.vue";
+import CrudTable from "./CrudTable.vue";
+import PropertyTable from "./PropertyTable.vue";
+import NavBar from "./NavBar.vue";
 
 function setup_components() {
-  Vue.component("h-confirm", Confirm);
-  Vue.component("h-form", Form);
-  Vue.component("h-array", ArrayList);
-  Vue.component("h-compare", Compare);
+  Vue.component("h-confirm", ConfirmDialog);
+  Vue.component("h-form", BasicForm);
+  Vue.component("h-array", ArrayTable);
+  Vue.component("h-compare", CompareTable);
   Vue.component("h-compare-entity", CompareEntity);
   Vue.component("h-search-form", SearchForm);
   Vue.component("h-edit-form", EditForm);
-  Vue.component("h-table", Table);
-  Vue.component("h-crud", Crud);
-  Vue.component("h-property", Property);
-  Vue.component("h-nav-bar", Navbar);
+  Vue.component("h-table", DataTable);
+  Vue.component("h-crud", CrudTable);
+  Vue.component("h-property", PropertyTable);
+  Vue.component("h-nav-bar", NavBar);
 }
 
 export { setup_components, init_axios, get_url, axios_get, axios_post, axios_cached_get, axios_download, axios_upload, is_success_response, is_error_response, is_been_referred, is_duplicated, has_invalid_params, is_no_session, save_entity, read_entity, read_entity_properties, list_entity, delete_entity, get_ref_labels, get_entity_meta, register_type, get_type, no_value, is_int };
