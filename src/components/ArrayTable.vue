bff82c7542d5ab2f5560589a8ff5e5437e6d6d9e
hery1979 <hery1979@qq.com>
2023-06-01 21:44:03 +0800

    add actions to compare table

--- src/components/ArrayTable.vue
+++ src/components/ArrayTable.vue
@@ -3,7 +3,23 @@
     <v-toolbar :class="toolbarClass" dark v-if="showToolbar">
       <v-text-field v-model="search" append-icon="mdi-magnify" :label="search_hint" single-line hide-details clearable></v-text-field>
     </v-toolbar>
-    <v-data-table v-bind="$attrs" v-on="$listeners" :headers="table_headers" :items="items" :search="search" disable-pagination hide-default-footer fixed-header :custom-filter="regex_search"> </v-data-table>
+    <v-data-table v-bind="$attrs" v-on="$listeners" :headers="table_headers" :items="items" :search="search" disable-pagination hide-default-footer fixed-header :custom-filter="regex_search">
+      <template v-slot:[`item._action`]="{ item }">
+        <v-tooltip v-for="(action, index) in actions" bottom v-bind:key="index">
+          <template v-slot:activator="{ on }">
+            <v-btn icon @click.stop="click_action(action, item)" v-on="on" v-show="!action.shown || action.shown(item)" :loading="icon_loading[item._id + action.icon]" :disabled="icon_loading[item._id + action.icon]">
+              <v-icon :color="action.color">{{ action.icon }}</v-icon>
+            </v-btn>
+          </template>
+          <template v-slot:loader>
+            <span class="custom-loader">
+              <v-icon light>mdi-cached</v-icon>
+            </span>
+          </template>
+          <span>{{ action.tooltip }}</span>
+        </v-tooltip>
+      </template>
+    </v-data-table>
   </v-card>
 </template>
 
@@ -17,6 +33,7 @@ export default {
 
   props: {
     objs: { type: Array, required: true },
+    hiddenProperties: { type: Array, default: () => [] },
     headerWidth: { type: String, default: "120px" },
     //Available options are start, center, end, baseline and stretch.
     headerAlign: { type: String, default: "center" },
@@ -25,6 +42,14 @@ export default {
     showToolbar: { type: Boolean, default: false },
     toolbarClass: { type: String, default: "app_bar subtitle-2" },
     searchHint: { type: String },
+
+    // This setting is used for action
+    //action for the item, such as delete item or edit item
+    actions: { type: Array, default: () => [] },
+    actionWidth: { type: String, default: "120px" },
+    //Available options are start, center, end, baseline and stretch.
+    actionAlign: { type: String, default: "start" },
+    actionClass: { type: String, default: "table_header subtitle-2" },
   },
 
   data() {
@@ -32,6 +57,7 @@ export default {
       search: "",
       items: [],
       table_headers: [],
+      icon_loading: {},
     };
   },
 
@@ -64,6 +90,20 @@ export default {
       return [...new Set(properties)];
     },
 
+    async click_action(action, item) {
+      if (action.animate) {
+        this.icon_loading[item._id + action.icon] = true;
+        await action.handle(item);
+        this.icon_loading[item._id + action.icon] = false;
+      } else {
+        action.handle(item);
+      }
+    },
+
+    uppcase_header(header_title) {
+      return this.headerUppcase ? header_title.toUpperCase() : header_title;
+    },
+
     parse_data() {
       if (!this.objs || this.objs.length == 0) {
         return;
@@ -71,9 +111,17 @@ export default {
 
       const headers = [];
       const items = [];
-      const properties = this.merge_properties();
+      const properties = this.merge_properties().filter((o) => !this.hiddenProperties.includes(o) && o != "_id");
       for (const property of properties) {
-        headers.push({ text: this.headerUppcase ? property.toUpperCase() : property, value: property, width: this.headerWidth, align: this.headerAlign, class: this.headerClass });
+        headers.push({ text: this.uppcase_header(property), value: property, width: this.headerWidth, align: this.headerAlign, class: this.headerClass });
+      }
+
+      if (this.actions.length > 0) {
+        const action = { text: this.uppcase_header(this.$t("table.action_header")), value: "_action", sortable: false };
+        action.width = this.actionWidth;
+        action.align = this.actionAlign;
+        action.class = this.actionClass;
+        headers.push(action);
       }
 
       for (const obj of this.objs) {
