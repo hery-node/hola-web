12b70a91190b5e97852275e13a6ac020dc1c9ad7
Heyuan Liu <heyuan.liu@intel.com>
2023-11-13 19:04:06 +0800

    for chart merge, show no value

--- src/core/chart.js
+++ src/core/chart.js
@@ -38,11 +38,11 @@ const merge_chart_data = (arr1, arr2) => {
     const arr2_cols = arr2[0].length;
     for (let i = 0; i < max; i++) {
         if (!has_value(arr1[i])) {
-            arr1[i] = [...new Array(arr1_cols)].map(() => 0);
+            arr1[i] = [...new Array(arr1_cols)].map(() => "");
             arr1[i][0] = arr2[i][0];
         }
         if (!has_value(arr2[i])) {
-            arr2[i] = [...new Array(arr2_cols)].map(() => 0);
+            arr2[i] = [...new Array(arr2_cols)].map(() => "");
         }
         arr1[i] = [...arr1[i], ...arr2[i].splice(1)];
     }
